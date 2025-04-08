511295c251f77fdd1b9f6474fb614cf30119db0c
Vaibhav Gupta <vaibhavgupta.v890@gmail.com>
2025-04-08 10:09:10 +0530

    Update page.tsx

--- app/src/app/patient/login/page.tsx
+++ app/src/app/patient/login/page.tsx
@@ -90,7 +90,7 @@ const PatientLoginPage: React.FC = () => {
               </button>
             </div>
           </div>
-
+        <div>Demo Room Number : 54</div>
        
           {/* Login Button */}
           <button
